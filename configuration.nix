7e1879fe7898c50e5d149fe9c3c3bef454ff8aaa
asarra <20255127+asarra@users.noreply.github.com>
2026-08-17 20:04:33 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -43,11 +43,10 @@ in
 
     # Kernel
     kernelPackages = pkgs.linuxPackages_latest;
-    kernelParams = [ "vfio-pci.ids=10de:1e84,10de:10f8" "amd_iommu=on" "iommu=pt" ] ++ [ "quiet" ] ++ [ "zswap.enabled=1" "zswap.max_pool_percent=25" "zswap.shrinker_enabled=1" ]; # https://www.kernel.org/doc/Documentation/admin-guide/kernel-parameters.txt
+    kernelParams = [ "vfio-pci.ids=10de:1e84,10de:10f8" "amd_iommu=on" "iommu=pt" ] ++ [ "quiet" ] ++ [ "zswap.enabled=1" "zswap.max_pool_percent=25" "zswap.shrinker_enabled=1" ]; # https://www.kernel.org/doc/Documentation/admin-guide/kernel-parameters.txt # https://ryantm.github.io/nixpkgs/functions/library/lists
     kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ] ++ config.boot.initrd.availableKernelModules ++ config.boot.initrd.kernelModules; # See: https://github.com/triton/triton
     kernel.sysctl = { "vm.swappiness" = 40; "vm.page-cluster" = 2; }; # swap specific kernel tunables
     blacklistedKernelModules = [ "nvidia" "nouveau" ];
-    initrd.availableKernelModules = (lib.subtractLists ["sr_mod" "rtsx_pci_sdmmc"] config.boot.initrd.availableKernelModules); # No "sr_mod" (CDs) & "rtsx_pci_sdmmc" (m. SD cards) https://ryantm.github.io/nixpkgs/functions/library/lists
     initrd.verbose = false;
   };
 
