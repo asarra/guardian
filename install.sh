e9765e32667b380a06a8212ebc3a9f7dc2f906d1
asarra <20255127+asarra@users.noreply.github.com>
2026-08-18 17:49:57 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,5 @@
 # Run as root
 bootctl remove
 nixos-rebuild switch # Making sure configuration.nix has been applied
-virsh net-autostart default # Autostarts internet connection for VMs
+bash ./deploy.sh
 reboot # Restarting since some services only take action after reboot. Even on Nix
