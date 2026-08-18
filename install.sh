3611738d7262655702df190de6ca98a19831ed69
asarra <20255127+asarra@users.noreply.github.com>
2026-08-18 07:31:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,5 +3,4 @@
 bootctl remove
 nixos-rebuild switch # Making sure configuration.nix has been applied
 virsh net-autostart default # Autostarts internet connection for VMs
-#git config --global user.name "asarra"
 reboot # Restarting since some services only take action after reboot. Even on Nix
