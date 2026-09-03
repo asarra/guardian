bacf5b5df7c3868f87fd14aaf654f8202fd7ede0
asarra <20255127+asarra@users.noreply.github.com>
2026-09-03 12:25:25 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,6 +2,7 @@
 # Run as root
 bootctl remove
 nixos-rebuild switch # Making sure configuration.nix has been applied
+SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 chmod +x "${SCRIPT_DIR}/deploy.sh"
 bash "${SCRIPT_DIR}/deploy.sh"
 reboot # Restarting since some services only take action after reboot. Even on Nix
