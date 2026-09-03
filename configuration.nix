a5a7024b73a1c1c380210554432e0c5cc7261131
asarra <20255127+asarra@users.noreply.github.com>
2026-09-03 14:22:35 +0200

    Update configuration.nix

--- configuration.nix
+++ configuration.nix
@@ -48,7 +48,7 @@ in
     # Kernel
     kernelPackages = pkgs.linuxPackages_latest;
     kernelParams = [ "vfio-pci.ids=10de:1e84,10de:10f8,10de:1ad8,10de:1ad9" "amd_iommu=on" "iommu=pt" ] ++ [ "quiet" ] ++ [ "zswap.enabled=1" "zswap.max_pool_percent=25" "zswap.shrinker_enabled=1" ]; # https://www.kernel.org/doc/Documentation/admin-guide/kernel-parameters.txt # https://ryantm.github.io/nixpkgs/functions/library/lists
-    kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ] ++ [ "bridge" "nft_chain_nat" "sch_htb" "cls_u32" "act_csum" ] ++ config.boot.initrd.availableKernelModules ++ config.boot.initrd.kernelModules; # See: https://github.com/triton/triton
+    kernelModules = [ "vfio_pci" "vfio" "vfio_iommu_type1" ] ++ config.boot.initrd.availableKernelModules ++ config.boot.initrd.kernelModules; # See: https://github.com/triton/triton
     kernel.sysctl = { "vm.swappiness" = 40; "vm.page-cluster" = 2; }; # swap specific kernel tunables
     blacklistedKernelModules = [ "nvidia" "nouveau" "nvidia_drm" "nvidia_modeset" "ucsi_ccg" ];
     initrd.kernelModules = [ "vfio_pci" "vfio_iommu_type1" "vfio" ];
@@ -141,7 +141,6 @@ in
 
   security = {
     protectKernelImage = true;
-    lockKernelModules = true;
     apparmor.enable = true;
     forcePageTableIsolation = true; # against meltdown and spectre attacks
     virtualisation.flushL1DataCache = "always";
