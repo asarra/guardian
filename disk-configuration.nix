1733e268ce2940f81e79fbe64ce7f4bb0a16475c
asarra <20255127+asarra@users.noreply.github.com>
2026-09-06 15:28:48 +0200

    Add files via upload

---
+++ disk-configuration.nix
@@ -0,0 +1,55 @@
+{
+  disko.devices = {
+    disk = {
+      # primary (SSD)
+      sdb = {
+        type = "disk";
+        device = "/dev/sdb";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              priority = 1;
+              name = "ESP";
+              size = "1G";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [ "fmask=0077" "dmask=0077" ];
+              };
+            };
+            root = {
+              priority = 2;
+              name = "root";
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/";
+              };
+            };
+          };
+        };
+      };
+      sda = {
+        type = "disk";
+        device = "/dev/sda";
+        content = {
+          type = "gpt";
+          partitions = {
+            data = {
+              size = "100%";
+              content = {
+                type = "filesystem";
+                format = "ext4";
+                mountpoint = "/mnt/data";
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
---
+++ flake.nix
@@ -0,0 +1,47 @@
+{
+  description = "Guardian Hypervisor - Public Flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
+
+    disko.url = "github:nix-community/disko";
+    disko.inputs.nixpkgs.follows = "nixpkgs";
+  };
+
+  outputs = { self, nixpkgs }: {
+    nixosConfigurations.installerISO = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        "${nixpkgs}/nixos/modules/installer/cd-dvd/installation-cd-minimal.nix"
+
+        ({ pkgs, ... }: {
+          networking.wireless = {
+            enable = true;
+            networks."${builtins.getEnv "WLAN_SSID"}".password = builtins.getEnv "WLAN_PASS";
+          };
+
+          services.openssh.enable = true;
+
+          users.users.guardian.password = builtins.getEnv "SSH_PASS";
+
+          environment.systemPackages = [
+            (pkgs.writeScriptBin "bootstrap" ''
+              #!/usr/bin/env bash
+              set -e
+              reboot
+            '')
+          ];
+        })
+      ];
+    };
+
+    nixosConfigurations.guardian = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        disko.nixosModules.disko
+        ./disk-configuration.nix
+        ./configuration.nix
+      ];
+    };
+  };
+}
