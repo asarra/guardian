ca8a1a1a5ec90d8896b6be33646cd2253a2d5884
asarra <20255127+asarra@users.noreply.github.com>
2026-09-06 18:45:10 +0200

    Delete install.sh

--- install.sh
+++
@@ -1,5 +0,0 @@
-#!/usr/bin/env bash
-# Run as root
-bootctl remove
-nixos-rebuild switch # Making sure configuration.nix has been applied
-reboot # Restarting since some services only take action after reboot. Even on Nix
